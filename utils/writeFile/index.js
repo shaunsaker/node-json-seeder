0dc478e9878de404a5d2699e618cdcb748c8e370
shaunsaker <shaun@aux.co.za>
2018-07-07 07:31:20 +0200

    Update writeFile util

--- utils/writeFile/index.js
+++ utils/writeFile/index.js
@@ -3,7 +3,7 @@ import fs from "fs";
 export default function writeFile(path, data, onError, onSuccess) {
   fs.writeFile(path, JSON.stringify(data), "utf8", error => {
     if (error) {
-      console.log(error);
+      onError(error);
     }
 
     onSuccess();
