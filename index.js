6ea0f79c8a4c88fee40ba87a96e7ec1d8f329603
shaunsaker <shaun@aux.co.za>
2018-07-07 07:43:16 +0200

    The good stuff

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Test');
+import writeFile from './utils/writeFile';
+
+const data = {};
+const rows = process.argv[2];
+
+for (let i = 0; i < rows; i += 1) {
+  const item = {
+    name: Math.random()
+      .toString(36)
+      .substr(2, 5), // generates random 5 letter string // TODO: util (any length)
+    race_id: Math.floor(100000 + Math.random() * 900000), // generates random 6 digit number // TODO: util (any length)
+  };
+
+  data[i] = item;
+}
+
+writeFile(
+  './output/data.json',
+  data,
+  (error) => {
+    console.log(error);
+  },
+  () => {
+    console.log('Success');
+  },
+);
